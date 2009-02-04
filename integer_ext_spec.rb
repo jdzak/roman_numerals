b4a2ed32540c45157cf390e81e7bccf4ebdeed03
John Dzak <johndzak@poop-2.local>
2009-02-04 10:22:32 -0600

    Add more specs and am extending math class to give ability to change the base of the log function.

--- integer_ext_spec.rb
+++ integer_ext_spec.rb
@@ -5,5 +5,8 @@ describe Integer, "to roman" do
   it "should convert the number 3 into III" do
     3.to_roman.should == "III"
   end
-  
+
+  it "should convert the number 7 into VII" do
+    7.to_roman.should == "VII"
+  end
 end
---
+++ math_ext_spec.rb
@@ -0,0 +1,4 @@
+describe Math, "log" do
+  it "should return 1 when log 10 with a base of 10 is given"
+  it "should return 2 when log 10 with a base of 5 is given"
+end
