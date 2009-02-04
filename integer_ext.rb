a5449beefa7517b6cb3a4302895e2254ec78ce40
John Dzak <johndzak@poop-2.local>
2009-02-04 11:07:16 -0600

    Progress so far.. 1 test out of 2 working.

--- integer_ext.rb
+++ integer_ext.rb
@@ -1,5 +1,18 @@
 class Integer
+  ROMAN_EQUIVALENTS = {1 => 'I', 5 => 'V', 10 => 'X', 50 => 'L', 100 => 'C', 500 => 'D', 1000 => 'M'}
+  AVAILABLE_NUMBERS = ROMAN_EQUIVALENTS.keys.sort.reverse # Descending
+  
   def to_roman
-    'III'
-  end
+    number = self
+    roman = ''
+    while(number > 0)
+      AVAILABLE_NUMBERS.each do |available|
+        if number % available == 0
+          roman << ROMAN_EQUIVALENTS[available]
+          number -= available
+        end
+      end
+    end
+    roman
+  end  
 end
--- math_ext_spec.rb
+++
@@ -1,4 +0,0 @@
-describe Math, "log" do
-  it "should return 1 when log 10 with a base of 10 is given"
-  it "should return 2 when log 10 with a base of 5 is given"
-end
